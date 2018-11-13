e05cc84a00872917301e7b71510939ba5ada5b64
melnichenko1996 <djek678@gmail.com>
2018-11-13 15:54:23 +0200

    add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+
   root "home_page#index"
 
   resources :home_page, only: [:index]
