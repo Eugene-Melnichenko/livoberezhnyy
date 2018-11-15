074e738009ab32f85f722c2bd4d1d384fa5bbc84
melnichenko1996 <djek678@gmail.com>
2018-11-15 14:56:52 +0200

    new param and desine

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_admin!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy] 
 
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc)
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -26,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -35,12 +34,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
   private
+
+  def set_post
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :text)
   end
--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,5 @@
 class HomePageController < ApplicationController
   def index
+    @articles = Article.order(created_at: :desc).limit(3)
   end
 end
